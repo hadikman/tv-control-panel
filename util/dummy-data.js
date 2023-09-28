7ee1047d201e6b3a53ccdeb2876edcef2f450991
hadikman <hadikman@gmail.com>
2023-09-28 21:02:30 +0330

    add dummy data

---
+++ util/dummy-data.js
@@ -0,0 +1,71 @@
+export const zones = [
+  {name: 'زون یک', videosCount: [], slug: 'zone-1', id: 1},
+  {
+    name: 'زون دو',
+    videosCount: ['img-3', 'img-4', 'img-5'],
+    slug: 'zone-2',
+    id: 2,
+  },
+  {
+    name: 'زون سه',
+    videosCount: ['img-4', 'img-5', 'img-6', 'img-7', 'img-8'],
+    slug: 'zone-3',
+    id: 3,
+  },
+  {
+    name: 'زون چهار',
+    videosCount: [
+      'img-9',
+      'img-10',
+      'img-11',
+      'img-12',
+      'img-13',
+      'img-14',
+      'img-15',
+      'img-16',
+    ],
+    slug: 'zone-4',
+    id: 4,
+  },
+  {
+    name: 'زون پنج',
+    videosCount: ['img-17', 'img-18', 'img-19', 'img-20', 'img-21', 'img-22'],
+    slug: 'zone-5',
+    id: 5,
+  },
+  {
+    name: 'زون شش',
+    videosCount: [
+      'img-23',
+      'img-24',
+      'img-25',
+      'img-26',
+      'img-27',
+      'img-28',
+      'img-29',
+    ],
+    slug: 'zone-6',
+    id: 6,
+  },
+]
+
+export const videos = [
+  {
+    name: 'ویدئو یک',
+    thumbnails: ['vid-screenshot-1', 'vid-screenshot-2', 'vid-screenshot-3'],
+    duration: 870738,
+    id: 1,
+  },
+  {
+    name: 'ویدئو دو',
+    thumbnails: ['vid-screenshot-4', 'vid-screenshot-5', 'vid-screenshot-6'],
+    duration: 840738,
+    id: 2,
+  },
+  {
+    name: 'ویدئو سه',
+    thumbnails: ['vid-screenshot-7', 'vid-screenshot-8', 'vid-screenshot-9'],
+    duration: 860738,
+    id: 3,
+  },
+]
