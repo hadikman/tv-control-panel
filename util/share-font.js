9773c3828e418cbef0ef310c57c2c1335ebc253a
hadikman <hadikman@gmail.com>
2023-09-28 21:04:30 +0330

    add variables for font

--- util/share-font.js
+++ util/share-font.js
@@ -6,16 +6,19 @@ const VAZIRMATN_FONT = localFont({
       path: '../assets/fonts/Vazirmatn-FD-Light.woff2',
       weight: '300',
       style: 'normal',
+      variable: '--ff-300',
     },
     {
       path: '../assets/fonts/Vazirmatn-FD-Regular.woff2',
       weight: '400',
       style: 'normal',
+      variable: '--ff-400',
     },
     {
       path: '../assets/fonts/Vazirmatn-FD-Bold.woff2',
       weight: '700',
       style: 'normal',
+      variable: '--ff-700',
     },
   ],
 })
