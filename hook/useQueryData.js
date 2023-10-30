a9fb326d8efe3ce1e0350fab794b08441b93f7ab
hadikman <hadikman@gmail.com>
2023-10-30 17:07:41 +0330

    add config parameters for useQueryData

--- hook/useQueryData.js
+++ hook/useQueryData.js
@@ -1,7 +1,39 @@
 import axiosClient from 'util/axios-http'
 import {useQuery} from '@tanstack/react-query'
 
-function useQueryData({queryKey, url, body, enabled}) {
+function useQueryData({
+  url,
+  body,
+  axiosConfig,
+  queryKey,
+  queryFn,
+  cacheTime,
+  enabled,
+  networkMode,
+  initialData,
+  initialDataUpdatedAt,
+  keepPreviousData,
+  meta,
+  notifyOnChangeProps,
+  onError,
+  onSettled,
+  onSuccess,
+  placeholderData,
+  queryKeyHashFn,
+  refetchInterval,
+  refetchIntervalInBackground,
+  refetchOnMount,
+  refetchOnReconnect,
+  refetchOnWindowFocus,
+  retry,
+  retryOnMount,
+  retryDelay,
+  select,
+  staleTime,
+  structuralSharing,
+  suspense,
+  useErrorBoundary,
+}) {
   const {
     data,
     dataUpdatedAt,
@@ -30,8 +62,35 @@ function useQueryData({queryKey, url, body, enabled}) {
     status,
   } = useQuery({
     queryKey: queryKey,
-    queryFn: () => axiosClient.post(url, body).then(res => res.data),
+    queryFn:
+      queryFn ||
+      (() => axiosClient.post(url, body, axiosConfig).then(res => res.data)),
+    cacheTime,
     enabled,
+    networkMode,
+    initialData,
+    initialDataUpdatedAt,
+    keepPreviousData,
+    meta,
+    notifyOnChangeProps,
+    onError,
+    onSettled,
+    onSuccess,
+    placeholderData,
+    queryKeyHashFn,
+    refetchInterval,
+    refetchIntervalInBackground,
+    refetchOnMount,
+    refetchOnReconnect,
+    refetchOnWindowFocus,
+    retry,
+    retryOnMount,
+    retryDelay,
+    select,
+    staleTime,
+    structuralSharing,
+    suspense,
+    useErrorBoundary,
   })
 
   return {
